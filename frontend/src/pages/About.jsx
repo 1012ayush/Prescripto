5da0588360f24b7327cd9e510148889baaec1748
1012ayush <ayush.24bce10624@vitbhopal.ac.in>
2026-06-15 23:53:13 +0530

    feat:functionality addition to About Page

--- frontend/src/pages/About.jsx
+++ frontend/src/pages/About.jsx
@@ -1,8 +1,44 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+      <div className='text-center text-2xl pt-10 text-gray-500 '>
+     <p>ABOUT <span className="text-gray-700 font-medium">US</span></p>
+     </div>
+
+     <div className="my-10 flex flex-col md:flex-row gap-12">
+      <img  className="w-full md:max-w-[360px] " src={assets.about_image} alt="" />
+      <div className="flex flex-col justify-center gap-6 md:w-2/4 text-sm text-gray-600">
+        <p>Welcome To Prescripto, Your Trusted Partner In Managing Your Healthcare Needs Conveniently And Efficiently.At Prescripto, We Understand The Challenges Individuals Face When It Comes To Scheduling Doctor Appointments And Managing Their Health Records.</p>
+        <p>Prescripto Is Committed To Excellence In Healthcare Technology. We Continuously Strive To Enhance Our Platform, Integrating The Latest Advancements To Improve User Experience And Deliver Superior Service.Whether You’re Booking Your First Appointment Or Managing Ongoing Care, Prescripto Is Here To Support You Every Step Of The Way.</p>
+        <b className="text-gray-800 ">Our Vision</b>
+        <p>Our Vision At Prescripto Is To Create A Seamless Healthcare Experience For Every User. We Aim To Bridge The Gap Between Patients And Healthcare Providers, Making It Easier For You To Access The Care You Need, When You Need It.</p>
+      </div>
+     </div>
+
+<div>
+  <p className="text-xl my-4">
+    WHY <span className="text-gray-700 font-semibold">CHOOSE US</span>
+  </p>
+</div>
+
+<div>
+  <div>
+    <b>EFFICIENCY</b>
+    <p>Streamlined Appointment Scheduling That Fits Into Your Busy Lifestyle.</p>
+  </div>
+  <div>
+    <b>CONVENIENCE</b>
+    <p>Access To A Network Of Trusted Healthcare Professional In Your Area.</p>
+  </div>
+  <div>
+    <b>PERSONALIZATION</b>
+    <p>Tailored Recommendation And Reminders To Help You Stay On Top Of Your Health.</p>
+  </div>
+</div>
+      </div>
   )
 }
 
