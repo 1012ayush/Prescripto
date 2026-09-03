42286e22541e23c6d1a3623d183b3318591a2040
1012ayush <ayush.24bce10624@vitbhopal.ac.in>
2026-09-03 20:13:37 +0530

    feat: implement doctor list API, frontend context integration, and update backend environment port

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -1,5 +1,7 @@
-import { createContext } from "react"; // it basically used to create empty data pipeline
+import { createContext, useEffect, useState } from "react"; // it basically used to create empty data pipeline
 import { doctors } from "../assets/assets";
+import axios from 'axios'
+import { toast } from "react-toastify";
 
 export const AppContext = createContext() // saved that empty pipeline with the variable and export 
 
@@ -7,10 +9,36 @@ const AppContextProvider = (props) => {  // it is created as if we do normal pro
 
     const currencySymbol = '$'
 
+    const backendUrl = import.meta.env.VITE_BACKEND_URL
+
+    const [doctors , setDoctors] = useState([])
+
     const value = {
         doctors,currencySymbol  // anything we will put in the value it is broadcasted to entire application , 
     }
 
+
+    const getDoctorsData = async () => {
+        try {
+            const {data} = await axios.get(backendUrl + '/api/doctor/list')
+
+            if(data.success) {
+                setDoctors(data.doctors)
+            }
+            else{
+                toast.error(data.message)
+            }
+
+        }catch (error) {
+            console.log(error)
+            toast.error(error.message)
+        }
+    }
+
+
+    useEffect(()=>{
+        getDoctorsData()
+    },[])
     return (
         <AppContext.Provider value={value}> {/* .provider is the transmitter */}
             {props.children} {/* Whatever components are wrapped inside  render them right here */}
